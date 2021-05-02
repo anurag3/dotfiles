d3320facdf8644dfb56be1ca853c02cc11eec5b0
Anurag Desai <adesai@wpi.edu>
2021-05-02 14:37:44 -0400

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,194 @@
+# Ask for the administrator password upfront
+sudo -v
+
+###############################################################################
+# General                                                                     #
+###############################################################################
+echo "Updating General Settings"
+
+read -p "Are you sure? " -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # Disable the sound effects on boot
+    sudo nvram SystemAudioVolume=" "
+
+    # Disable transparency in the menu bar and elsewhere on Yosemite
+    # defaults write com.apple.universalaccess reduceTransparency -bool true
+
+    # Set sidebar icon size to medium
+    defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+
+    # Always show scrollbars
+    # defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
+    # Possible values: `WhenScrolling`, `Automatic` and `Always`
+
+    # Expand save panel by default
+    defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+    defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+    # Expand print panel by default
+    defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+    defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+    # Save to disk (not to iCloud) by default
+    defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+    # Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+    /System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+    # Disable automatic capitalization as it’s annoying when typing code
+    defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+    # Disable smart dashes as they’re annoying when typing code
+    defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+    # Disable automatic period substitution as it’s annoying when typing code
+    defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
+    # Disable smart quotes as they’re annoying when typing code
+    defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+    # Disable auto-correct
+    defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+fi
+
+###############################################################################
+# Keyboard                                                                    #
+###############################################################################
+echo "Updating Keyboard Settings"
+read -p "Are you sure? " -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # Enable full keyboard access for all controls
+    # (e.g. enable Tab in modal dialogs)
+    defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+    # Disable press-and-hold for keys in favor of key repeat
+    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+    # Set a blazingly fast keyboard repeat rate
+    defaults write NSGlobalDomain KeyRepeat -int 1
+    defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+    # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+    defaults write com.apple.screencapture type -string "png"
+fi
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+echo "Updating Finder Settings"
+read -p "Are you sure? " -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # Finder: show all filename extensions
+    defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+    # Finder: show path bar
+    defaults write com.apple.finder ShowPathbar -bool true
+fi
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+echo "Updating Dock Settings"
+read -p "Are you sure? " -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # Set the icon size of Dock items to 36 pixels
+    defaults write com.apple.dock tilesize -int 36
+
+    # Change minimize/maximize window effect
+    defaults write com.apple.dock mineffect -string "scale"
+
+    # Minimize windows into their application’s icon
+    defaults write com.apple.dock minimize-to-application -bool true
+
+    # Show indicator lights for open applications in the Dock
+    defaults write com.apple.dock show-process-indicators -bool true
+
+    # Remove the auto-hiding Dock delay
+    defaults write com.apple.dock autohide-delay -float 0
+    # Remove the animation when hiding/showing the Dock
+    defaults write com.apple.dock autohide-time-modifier -float 0
+
+    # Automatically hide and show the Dock
+    defaults write com.apple.dock autohide -bool true
+
+    # Make Dock icons of hidden applications translucent
+    defaults write com.apple.dock showhidden -bool true
+
+    # Don’t show recent applications in Dock
+    defaults write com.apple.dock show-recents -bool false
+
+    # Kill and restart Dock
+    killall Dock
+fi
+###############################################################################
+# Activity Monitor                                                            #
+###############################################################################
+echo "Updating Activity Monitor Settings"
+read -p "Are you sure? " -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    # Show the main window when launching Activity Monitor
+    defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+    # Visualize CPU usage in the Activity Monitor Dock icon
+    defaults write com.apple.ActivityMonitor IconType -int 5
+
+    # Show all processes in Activity Monitor
+    defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+    # Sort Activity Monitor results by CPU usage
+    defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+    defaults write com.apple.ActivityMonitor SortDirection -int 0
+fi
+###############################################################################
+# Development                                                                 #
+###############################################################################
+echo "Updating Development Settings"
+
+echo "Installing Xcode"
+xcode-select --install
+
+# Check for Homebrew,
+# Install if we don't have it
+echo "Installing Homebrew"
+if test ! $(which brew); then
+echo "Installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update homebrew recipes
+echo "Updating Homebrew"
+brew update
+
+echo "Installing Git"
+brew install git
+
+echo "Updating Git Config"
+git config --global user.name "Anurag Desai"
+git config --global user.email adesai@wpi.edu
+
+echo "Cleaning up brew"
+brew cleanup
+
+#Install Zsh
+echo "Installing Oh My ZSH"
+curl -L http://install.ohmyz.sh | sh
+
+echo "Installing ZSH Theme - Spaceship"
+cd ~/code;
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1;
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+echo "Installing Pyenv"
+brew update
+brew install pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
