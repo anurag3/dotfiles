89479a2a3d6557ddc386f061930bc327870691fa
Anurag Desai <anurag.desai@hginsights.com>
2026-05-28 08:33:06 -0400

    Add Spotlight search configuration to setup.sh

--- setup.sh
+++ setup.sh
@@ -72,6 +72,35 @@ defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"  # Sort b
 defaults write com.apple.ActivityMonitor SortDirection -int 0            # Descending (highest CPU first)
 
 ###############################################################################
+# Spotlight                                                                   #
+###############################################################################
+echo "==> Configuring Spotlight..."
+defaults write com.apple.spotlight orderedItems -array \
+    '{"enabled" = 1; "name" = "APPLICATIONS";}' \
+    '{"enabled" = 1; "name" = "MENU_EXPRESSION";}' \
+    '{"enabled" = 0; "name" = "CONTACT";}' \
+    '{"enabled" = 0; "name" = "MENU_CONVERSION";}' \
+    '{"enabled" = 1; "name" = "MENU_DEFINITION";}' \
+    '{"enabled" = 0; "name" = "SOURCE";}' \
+    '{"enabled" = 0; "name" = "DOCUMENTS";}' \
+    '{"enabled" = 0; "name" = "EVENT_TODO";}' \
+    '{"enabled" = 0; "name" = "DIRECTORIES";}' \
+    '{"enabled" = 0; "name" = "FONTS";}' \
+    '{"enabled" = 0; "name" = "IMAGES";}' \
+    '{"enabled" = 0; "name" = "MESSAGES";}' \
+    '{"enabled" = 0; "name" = "MOVIES";}' \
+    '{"enabled" = 0; "name" = "MUSIC";}' \
+    '{"enabled" = 0; "name" = "MENU_OTHER";}' \
+    '{"enabled" = 0; "name" = "PDF";}' \
+    '{"enabled" = 0; "name" = "PRESENTATIONS";}' \
+    '{"enabled" = 0; "name" = "MENU_SPOTLIGHT_SUGGESTIONS";}' \
+    '{"enabled" = 0; "name" = "SPREADSHEETS";}' \
+    '{"enabled" = 1; "name" = "SYSTEM_PREFS";}' \
+    '{"enabled" = 0; "name" = "TIPS";}' \
+    '{"enabled" = 0; "name" = "BOOKMARKS";}'
+killall Spotlight 2>/dev/null || true
+
+###############################################################################
 # Xcode CLI Tools                                                             #
 ###############################################################################
 echo "==> Installing Xcode CLI tools..."
